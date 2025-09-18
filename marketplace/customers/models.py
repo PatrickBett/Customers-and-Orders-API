cbd413e411fd279e1ca21c828ccc561651beb822
Patrick <patrickbett018@gmail.com>
2025-09-18 23:21:34 +0300

    first commit

---
+++ marketplace/customers/models.py
@@ -0,0 +1,21 @@
+import uuid
+from django.db import models
+
+class Customer(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=100)
+    code = models.CharField(max_length=50, unique=True)
+    phone_number = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.name
+
+class Order(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    item = models.CharField(max_length=100)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    order_time = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.item} - {self.customer.name}"
