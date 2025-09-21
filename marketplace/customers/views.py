f8f60c4fccb944ca67213d6b3e2b74483741f760
Patrick <patrickbett018@gmail.com>
2025-09-21 10:55:58 +0300

    authentication setup

--- marketplace/customers/views.py
+++ marketplace/customers/views.py
@@ -5,13 +5,17 @@ from rest_framework import status, generics
 from .models import Customer, Order
 from .serializers import CustomerSerializer, OrderSerializer
 from rest_framework.decorators import api_view
-
+from rest_framework.permissions import IsAuthenticated
 
 # Using APIView
 @api_view(['GET'])
 def home(request):
     return Response({"message": "Customer Orders API"})
+
+
 class CustomerListCreateAPIView(APIView):
+    permission_classes = [IsAuthenticated]
+
     def get(self, request):
         customers = Customer.objects.all()
         serializer = CustomerSerializer(customers, many=True)
@@ -24,7 +28,19 @@ class CustomerListCreateAPIView(APIView):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-# Using generic views for simplicity
-class OrderListCreateAPIView(generics.ListCreateAPIView):
-    queryset = Order.objects.all()
-    serializer_class = OrderSerializer
+# Orders
+class OrderListCreateAPIView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        orders = Order.objects.all()
+        serializer = OrderSerializer(orders, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = OrderSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            # can call SMS sending logic
+            return Response(serializer.data, status=201)
+        return Response(serializer.errors, status=400)
--- marketplace/marketplace/urls.py
+++ marketplace/marketplace/urls.py
@@ -3,6 +3,7 @@ from django.contrib import admin
 from django.urls import path, include
 from customers.views import home
 urlpatterns = [
+    path('oidc/', include('mozilla_django_oidc.urls')),  # login, callback, logout
     path('admin/', admin.site.urls),
     path('api/', include('customers.urls')),
     path('',home, name='home')
