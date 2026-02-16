00d9631dc6cc1e13eddba791c14b63870c9bf0d4
PatrickBett <patrickbett018@gmail.com>
2026-02-16 20:00:20 +0300

    commit

--- marketplace/customers/models.py
+++ marketplace/customers/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import AbstractUser
 class CustomUser(AbstractUser):
     code = models.CharField(max_length=50, unique=True) 
     account_balance = models.DecimalField(max_digits=10, decimal_places=2, default=10000.00)
-    profile_picture = models.URLField(blank=True, null=True)
+    profile_picture = models.URLField(max_length=1024,blank=True, null=True)
     def __str__(self):
         return self.username
 
